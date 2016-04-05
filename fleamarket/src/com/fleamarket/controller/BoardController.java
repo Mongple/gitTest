fa53d350f83d697e445ba9b591730831f841e49b
feel4387 <feel4387@fdac61e1-da2e-0510-93eb-a30064785d52>
2016-04-05 05:33:22 +0000

    url변경

--- fleamarket/src/com/fleamarket/controller/BoardController.java
+++ fleamarket/src/com/fleamarket/controller/BoardController.java
@@ -26,7 +26,7 @@ public class BoardController {
 	
 
 /*mainController
-@RequestMapping("/fleamarket")
+@RequestMapping("/market")
 public String goFleaMarket(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception {
 	vo.setBaType("WEAR");
 	List<BoardVO> list = service.selectList("board.selectBoardByType",vo,new RowBounds(vo.getOffset(), vo.getLimit()));
@@ -35,7 +35,7 @@ public String goFleaMarket(@ModelAttribute("BoardVO") BoardVO vo, HttpServletReq
 	return "/fleamarket/fleamarketlist";
 }*/
 
-	@RequestMapping("/fleamarket_prod")
+	@RequestMapping("/market/prod")/*/fleamarket_prod*/
 	public String goFleaMarketProd(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception {
 		vo.setBaType("PRODUCT");
 		List<BoardVO> list = service.selectList("board.selectBoardByType", vo, new RowBounds(vo.getOffset(), vo.getLimit()));
@@ -43,14 +43,14 @@ public String goFleaMarket(@ModelAttribute("BoardVO") BoardVO vo, HttpServletReq
 		req.setAttribute("vo", vo);
 		return "/fleamarket/fleamarketlist_prod";
 	}
-	@RequestMapping("/fleamarket_board_write")
+	@RequestMapping("/market/writeboard")/*/fleamarket_board_write*/
 	public String insertBoard(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception{
 		if((String)req.getSession().getAttribute("sessionId") == null )
 			return "redirect:/main";
 		req.setAttribute("vo", vo);
 		return "/fleamarket/fleamarket_board_write";
 	}
-	@RequestMapping("/fleamarket_board_write_action")
+	@RequestMapping("/market/writeboard/action")/*/fleamarket_board_write_action*/
 	public String insertBoardAction(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception {
 		vo.setMemId((String) req.getSession().getAttribute("sessionId"));
 		req.setAttribute("vo", vo);
@@ -59,79 +59,70 @@ public String goFleaMarket(@ModelAttribute("BoardVO") BoardVO vo, HttpServletReq
 
 			System.out.println("확인"+vo.getBaType());
 			if(vo.getBaType().equals("WEAR")){
-				return "redirect:/fleamarket";
+				return "redirect:/market";
 			}
 			else{
-				return "redirect:/fleamarket_prod";
+				return "redirect:/market/prod";
 			}
 		}
 		return "";
 	}
-	@RequestMapping("/fleamarket_board_edit")
+	@RequestMapping("/market/editboard")/*fleamarket_board_edit*/
 	public String editBoard(@ModelAttribute("BoardVO") BoardVO vo, Model model) throws Exception{
-		
 		vo = (BoardVO) service.selectOne("board.selectBoardByNo", vo);
 		model.addAttribute("vo", vo);
 		return "/fleamarket/fleamarket_board_edit";
 	}
-	@RequestMapping("/updateBoard")
+	@RequestMapping("/market/updateBoard")
 	public String updateBoard(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception {
-		
 		if(service.update("board.updateBoard",vo) != 0){
 			logger.debug("업데이트 성공");
-			return "redirect:/fleamarket_board?baNo="+vo.getBaNo();
+			return "redirect:/market/board?baNo="+vo.getBaNo();
 		}
 		return "";
 	}
-	@RequestMapping("/updateBaCount")
+	@RequestMapping("/market/updateBaCount")
 	public String updateBaCount(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception {
-		System.out.println("****updateBaCount***");
 		if(service.update("board.updateBaCount",vo) != 0){
 			logger.debug("조회수업데이트 성공");
-			return "redirect:/fleamarket_board?baNo="+vo.getBaNo();
+			return "redirect:/market/board?baNo="+vo.getBaNo();
 		}
 		return "";
 	}
-	@RequestMapping("/deleteBoard")
+	@RequestMapping("/market/deleteBoard")
 	public String deleteBoard(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception {	
 		req.setAttribute("vo", vo);
 		if(service.delete("board.deleteBoard",vo.getBaNo()) != 0){
 			logger.debug("삭제 성공");
-
-			return "redirect:/fleamarket";
-
+			return "redirect:/market";
 		}
 		return "";
 	}
 	
 	/* 댓글 */
-	@RequestMapping("/fleamarket_board")
+	@RequestMapping("/market/board")/*fleamarket_board*/
 	public String boardForm(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception{
 		vo = (BoardVO) service.selectOne("board.selectBoardByNo", vo);
-		
 		List<BoardVO> rplist = service.selectList("board.selectReplyByBaNo", vo, new RowBounds(vo.getOffset(), vo.getLimit()));
 		req.setAttribute("rplist",rplist);
-		
 		req.setAttribute("vo", vo);
 		return "/fleamarket/fleamarket_board";
 	}
-	@RequestMapping("/insertComment")
+	@RequestMapping("/market/insertComment")
 	public String insertComment(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception {
-		System.out.println("***insertComment");
 		vo.setMemId((String) req.getSession().getAttribute("sessionId"));
 		req.setAttribute("vo", vo);
 		if(service.insert("board.insertComment",vo) != 0){
 			logger.debug("등록성공");
-
-			return "redirect:/fleamarket_board?baNo="+vo.getBaNo();
+			return "redirect:/market/board?baNo="+vo.getBaNo();
 		}
 		return "";
 	}
-	@RequestMapping("/deleteComment")
+	@RequestMapping("/market/deleteComment")
 	public String deleteComment(@ModelAttribute("BoardVO") BoardVO vo, HttpServletRequest req) throws Exception {	
 		if(service.delete("board.deleteComment",vo) != 0){
 			logger.debug("댓글삭제 성공");
-			return "redirect:/fleamarket_board?baNo="+vo.getBaNo();
+			return "redirect:/market/board?baNo="+vo.getBaNo();
 		}
 		req.setAttribute("vo", vo);
 		return "";
