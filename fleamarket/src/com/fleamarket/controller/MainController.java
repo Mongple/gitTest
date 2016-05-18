3121a1b081a296e818d2cb32dbc7273136c938cf
TS0463 <TS0463@TS0463.tb.com>
2016-05-19 08:52:38 +0900

    git test

--- fleamarket/src/com/fleamarket/controller/MainController.java
+++ fleamarket/src/com/fleamarket/controller/MainController.java
@@ -27,7 +27,7 @@ public class MainController {
 	public String goMain() throws Exception {
 		return "redirect:/main";
 	}
-	
+	//git test
 	@RequestMapping("/main")
 	public String goMain2(HttpServletRequest req) throws Exception {
 		BoardVO wear = new BoardVO();
