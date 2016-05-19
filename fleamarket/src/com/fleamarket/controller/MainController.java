fe67f4567529fd8aeb2dc9424e75614206bd58d9
TS0463 <TS0463@TS0463.tb.com>
2016-05-19 18:57:40 +0900

    올라가랏!

--- fleamarket/src/com/fleamarket/controller/MainController.java
+++ fleamarket/src/com/fleamarket/controller/MainController.java
@@ -27,7 +27,7 @@ public class MainController {
 	public String goMain() throws Exception {
 		return "redirect:/main";
 	}
-	//git test ssss
+	//git test sssshvjhgjhghjg
 	@RequestMapping("/main")
 	public String goMain2(HttpServletRequest req) throws Exception {
 		BoardVO wear = new BoardVO();
