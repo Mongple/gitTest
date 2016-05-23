59e0fe5fecf969cc35df430c4589a2f3ce9a1495
TS0464 <TS0464@TS0464.tb.com>
2016-05-23 14:47:20 +0900

    테스트12

--- fleamarket/src/com/fleamarket/controller/AdminController.java
+++ fleamarket/src/com/fleamarket/controller/AdminController.java
@@ -22,7 +22,7 @@ public class AdminController {
 	Service service;
 	
 	Logger logger = LogManager.getLogger(this.getClass());
-	
+	//테스트입니다.
 	@RequestMapping("/admin/adminMain/rate")
 	public String viewAdminRate(HttpServletRequest req) throws Exception {
 		if(req.getSession().getAttribute("sessionId") == null || req.getSession().getAttribute("sessionGrant").equals("U")) {
